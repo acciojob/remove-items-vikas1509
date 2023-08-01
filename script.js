968859950040bb2c6910e67eac604108a7f54505
vikas1509 <119025906+vikas1509@users.noreply.github.com>
2023-08-01 08:28:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+       function removeColor() {
+		 //  let colorSelect = document.getElementById(colorSelect);
+		   
+     //   	  var selectedOption = colorSelect.options[colorSelect.selectedIndex];
+		   
+        // Remove the selected option from the dropdown list
+        colorSelect.remove(colorSelect.selectedIndex);
+    
+       }
